17b739c60ec035b2babe3ccc65de0c90c30dffaf
nicolendoruiz <nicolendoruiz@gmail.com>
2020-08-17 15:46:47 -0500

    quitar favorito completado

--- script/favoritos.js
+++ script/favoritos.js
@@ -15,6 +15,33 @@ async function cargarFavoritos(offset) {
             for (let i = 0; i < gifsFavoritos.data.length; i++) {
                 let div = document.createElement('div');
                 let img = document.createElement('img');
+                div.innerHTML = `<div id="${gifsFavoritos.data[i].id}" class="card-opciones">
+                                    <div class="opciones-gif">
+                                        <button id="btn-favorito" class="opcion-button">
+                                            <img src="images/icon-favoritos-fav.svg" alt="icono-favorito">
+                                        </button>
+                                        <button id="btn-descargar" class="opcion-button">
+                                            <img src="images/icon-download.svg" alt="icono-descarga">
+                                        </button>
+                                        <button id="btn-max" class="opcion-button">
+                                            <img src="images/icon-max.svg" alt="icono-maximizar">
+                                        </button>
+                                    </div>
+                                    <div class="opciones-descripcion">
+                                        <p class="descripcion user">${gifsFavoritos.data[i].username}</p>
+                                        <p class="descripcion titulo">${gifsFavoritos.data[i].title}</p>
+                                    </div>
+                                </div>`;
+                div.querySelector('#btn-favorito').addEventListener('click', () => {
+                    eliminarFavoritos(gifsFavoritos.data[i].id)
+                    // alert('Hola');
+                });
+                div.querySelector('#btn-descargar').addEventListener('click', () => {
+                    descargarGif(gifsFavoritos.data[i].images.original.url);
+                });
+                div.querySelector('#btn-max').addEventListener('click', () => {
+                    maximizarGif(gifsFavoritos.data[i].id);
+                });
                 img.srcset = `${gifsFavoritos.data[i].images.downsized_large.url}`;
                 img.alt = `${gifsFavoritos.data[i].id}`;
                 div.appendChild(img);
@@ -27,4 +54,11 @@ async function cargarFavoritos(offset) {
         contenedor_favoritos.classList.add('hidden');
         contenedor_sinfavoritos.classList.remove('hidden');
     }
+}
+
+function eliminarFavoritos(idGifFavorito) {
+    let indiceFavorito = listado_favoritos.indexOf(idGifFavorito);
+    listado_favoritos.splice(indiceFavorito, 1);
+    localStorage.setItem('gifsFavoritos', JSON.stringify(listado_favoritos));
+    cargarFavoritos();
 }
